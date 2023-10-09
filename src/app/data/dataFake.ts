2fdf4d9d151e4791019fbeb6b4088119d029f965
Iuridev <iurigames23@gmail.com>
2023-10-08 22:54:57 -0300

    blog inserindo dados dinamicamente

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,8 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Noticia Nova",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat facilis, quod qui delectus illum, maiores repellat molestias animi voluptate corrupti, voluptatem porro voluptas fugiat neque ipsa tenetur nesciunt sequi ea!",
+        "photoCover": "https://img.freepik.com/fotos-gratis/plano-de-fundo-de-programacao-com-pessoa-trabalhando-com-codigos-no-computador_23-2150010125.jpg?w=1060&t=st=1696811741~exp=1696812341~hmac=4e5fa94a89bf5af72d1cf451d3dccfa9eced7ec59f1ae7957b869a755a1d9d86"
+    }
+]
